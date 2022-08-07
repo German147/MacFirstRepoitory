9d5b3919d908d3a9fbef982cfb65ccab1e467e81
German <barreragerman27@gmail.com>
2022-08-07 11:06:52 -0300

    .idea was added to the ignored files

---
+++ src/main/java/org/BarreraGerman/App.java
@@ -0,0 +1,13 @@
+package org.BarreraGerman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!  My first app with a new mac" );
+    }
+}
